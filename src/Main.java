eca780a5d1cff98f01d9f163ff4ce4366644cc96
Ruslan Pavlov <rpavlov@ubic.tech>
2024-11-08 14:54:28 +0600

    Добавлен новый метод sayHello

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+
 }
